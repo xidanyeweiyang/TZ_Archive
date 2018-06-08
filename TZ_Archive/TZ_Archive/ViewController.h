5e023affa37a082637f5a4c096db393a1c24b194
xidanyeweiyang <ytliuming47455@sina.com>
2018-06-08 23:04:16 +0800

    提交

---
+++ TZ_Archive/TZ_Archive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TZ_Archive
+//
+//  Created by Cooci on 2018/6/8.
+//  Copyright © 2018年 Cooci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TZ_Archive/TZ_ArchiveTests/TZ_ArchiveTests.m
@@ -0,0 +1,39 @@
+//
+//  TZ_ArchiveTests.m
+//  TZ_ArchiveTests
+//
+//  Created by Cooci on 2018/6/8.
+//  Copyright © 2018年 Cooci. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TZ_ArchiveTests : XCTestCase
+
+@end
+
+@implementation TZ_ArchiveTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
